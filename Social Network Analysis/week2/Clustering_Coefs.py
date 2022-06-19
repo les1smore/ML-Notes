dbe83c072a4cafe60b0f5c68758651c0d6c3e5a8
Leslie <60702562+les1smore@users.noreply.github.com>
2022-06-19 18:55:53 -0400

    Update Clustering_Coefs.py

--- Social Network Analysis/week2/Clustering_Coefs.py
+++ Social Network Analysis/week2/Clustering_Coefs.py
@@ -1,1 +1,11 @@
 # Triadic closure: The tendency for people who share connections in a social network to become connected.
+
+G = nx.Graph()
+G.add_edges_from([('A','K),('A','B'),('A','C'),('B','C'),('B','K'),
+                  ('C','E'),('C','F'),('D','E'),('E','F'),('E','H'),
+                  ('F','G'),('I','J')])
+# Local clusterig coefficient for node F
+nx.clustering(G, 'F')
+                  
+# Global Clustering Coefficient
+                  
