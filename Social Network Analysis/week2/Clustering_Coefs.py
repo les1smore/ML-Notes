d3cf1df5daf79348634bd8b9f6c0830955bc9811
Leslie <60702562+les1smore@users.noreply.github.com>
2022-06-19 19:11:20 -0400

    Update Clustering_Coefs.py

--- Social Network Analysis/week2/Clustering_Coefs.py
+++ Social Network Analysis/week2/Clustering_Coefs.py
@@ -1,5 +1,5 @@
 # Triadic closure: The tendency for people who share connections in a social network to become connected.
-
+# Clustering coef: measures the degree to which in a network tend to 'cluster' or form triangles
 G = nx.Graph()
 G.add_edges_from([('A','K),('A','B'),('A','C'),('B','C'),('B','K'),
                   ('C','E'),('C','F'),('D','E'),('E','F'),('E','H'),
@@ -8,4 +8,12 @@ G.add_edges_from([('A','K),('A','B'),('A','C'),('B','C'),('B','K'),
 nx.clustering(G, 'F')
                   
 # Global Clustering Coefficient
+""" Measuring clustering on the whole network
+Approach 1: Average local clustering coefficient over all nodes in the Graph"""
+nx.average_clustering(G)
+
+"""Approcah2: percentaege of open triads that are triangles in a network"""
+# Transitivity: ratio of number of triangles and number of open triads in a network
+nx.transitivity(G)
+
                   
